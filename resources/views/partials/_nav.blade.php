e65b0263d3b571a98ef4ae2cd989368d7ad53b75
Owen Ekhator <oe52@gmail.com>
2018-02-03 20:02:04 -0500

    created views for authentication page

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -54,7 +54,7 @@
                 @endguest
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{route('posts.index')}}">Post</a>
-                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="{{route('categories.index')}}">Categories</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
